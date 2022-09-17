e3cab4310c77c01122eccdf92c412d791af73521
jamalTaha <113145154+jamalTaha@users.noreply.github.com>
2022-09-17 14:34:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,22 @@ const getHeader = document.querySelectorAll("header"),
    Read and update your HTML to discover the requirements.
    The tests will run every time you update your code.
    ===================
-*/
+*/function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(getNumberOrString(document.getElementById('text').value)));
+
+});
 function getNumberOrString(value) {
   // Convert a string value to a number if possible
   let number_value = Number(value);
